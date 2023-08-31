21ae8e6d7e7a85ceb22ff7731e09f27e9df0e52b
severuna <oxxxyfomenkova@gmail.com>
2023-08-31 18:21:34 +0300

    clear proj

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Main.css';
+
+const Main = () => {
+    return (
+        <main>
+            <h1>Main</h1>
+        </main>
+    );
+};
+
+export default Main;
