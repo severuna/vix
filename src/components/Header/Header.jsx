6844602e3494a7460bf9c5a1902729cd277d78f2
severuna <oxxxyfomenkova@gmail.com>
2023-09-02 19:09:40 +0300

    add header head (logo & init nav)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Header.css';
+import logo from '../../assets/img/logo.svg';
+import Nav from './Nav/Nav';
 
 const Header = () => {
     return (
         <header className='header col'>
-            <h1>header</h1>
+            <div className='header-head row'>
+                <img src={logo} alt='logo' className='logo' />
+                <Nav />
+            </div>
         </header>
     );
 };
---
+++ src/components/Header/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Nav.css';
+
+const Nav = () => {
+    return (
+        <nav className='nav col'>
+            nav
+        </nav>
+    );
+};
+
+export default Nav;
